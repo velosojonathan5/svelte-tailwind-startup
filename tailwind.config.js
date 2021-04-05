8d957e673e39d9c905c036aa62665010c178cd36
Jonathan Veloso <jonathan.veloso@rarolabs.com.br>
2021-04-05 12:23:09 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: {
+    enabled: !process.env.ROLLUP_WATCH,
+    content: ["./public/index.html", "./src/**/*.svelte"],
+    options: {
+      defaultExtractor: (content) => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
+      ],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "light-blue": colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
